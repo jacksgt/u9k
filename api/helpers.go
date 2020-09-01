457590ec1defb43c862f989ff671dbca4091bfad
Jack Henschel <jackdev@mailbox.org>
2020-09-01 15:49:40 +0200

    Front-end work

--- api/helpers.go
+++ api/helpers.go
@@ -23,6 +23,8 @@ func httpError(w http.ResponseWriter, message string, code int){
 	// 		"FOOTER": footer,
 	// 	})
 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(code)
 	fmt.Fprintf(w, "%d - %s\n", code, message)
 	return
 }
