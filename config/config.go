fba8ae56f7c473a6a9e6c64f44e62bd99eccde7e
Jack Henschel <jackdev@mailbox.org>
2020-09-16 13:24:52 +0200

    Prefix environment variables with U9K
    better interop with other applications

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Init() {
-	baseUrl := os.Getenv("BASE_URL")
+	baseUrl := os.Getenv("U9K_BASE_URL")
 	if baseUrl == "" {
 		baseUrl = "http://localhost:3000/"
 	}
@@ -15,43 +15,47 @@ func Init() {
 	}
 	BaseUrl = baseUrl
 
-	listenAddr := os.Getenv("LISTEN_ADDR")
+	listenAddr := os.Getenv("U9K_LISTEN_ADDR")
 	if listenAddr == "" {
 		listenAddr = "127.0.0.1"
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = os.Getenv("U9K_PORT")
+		if port == "" {
+			port = "3000"
+		}
 	}
 	HttpListenAddr = listenAddr + ":" + port
 
-	dbConnUrl := os.Getenv("DB_CONN_URL")
+	dbConnUrl := os.Getenv("U9K_DB_CONN_URL")
 	if dbConnUrl == "" {
 		dbConnUrl = "postgresql://root@localhost:26257/u9k?sslmode=disable"
 	}
 	DbConnUrl = dbConnUrl
 
-	S3Region = os.Getenv("S3_REGION")
+	S3Region = os.Getenv("U9K_S3_REGION")
 	if S3Region == "" {
 		S3Region = "us-east-1"
 	}
 
-	S3AccessKey = os.Getenv("S3_ACCESS_KEY")
+	S3AccessKey = os.Getenv("U9K_S3_ACCESS_KEY")
 	if S3AccessKey == "" {
-		log.Fatalf("No S3_ACCESS_KEY specified. Aborting.\n")
+		log.Fatalf("No U9K_S3_ACCESS_KEY specified. Aborting.\n")
 	}
 
-	S3SecretKey = os.Getenv("S3_SECRET_KEY")
+	S3SecretKey = os.Getenv("U9K_S3_SECRET_KEY")
 	if S3SecretKey == "" {
-		log.Fatalf("No S3_SECRET_KEY specified. Aborting.\n")
+		log.Fatalf("No U9K_S3_SECRET_KEY specified. Aborting.\n")
 	}
 
-	S3Bucket = os.Getenv("S3_BUCKET")
+	S3Bucket = os.Getenv("U9K_S3_BUCKET")
 	if S3Bucket == "" {
 		S3Bucket = "u9k-dev"
 	}
 
-	S3Endpoint = os.Getenv("S3_ENDPOINT")
+	S3Endpoint = os.Getenv("U9K_S3_ENDPOINT")
+	// defaults to AWS
 }
 
 var BaseUrl string
