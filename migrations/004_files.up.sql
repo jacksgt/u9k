e00c2431d87fd1cf377ec6cc8af1d79b85a8f5e0
Jack Henschel <jackdev@mailbox.org>
2020-09-20 15:55:40 +0200

    Set file ID length to 6 characters
    should be enough

--- migrations/004_files.up.sql
+++ migrations/004_files.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS files (
-       id STRING PRIMARY KEY DEFAULT (translate(substr(encode(gen_random_uuid()::bytes, 'base64'), 1, 10), '/+', '-_')),
+       id STRING PRIMARY KEY DEFAULT (translate(substr(encode(gen_random_uuid()::bytes, 'base64'), 1, 6), '/+', '-_')),
        filename STRING,
        filetype STRING,
        create_ts TIMESTAMP DEFAULT NOW()::timestamp,
